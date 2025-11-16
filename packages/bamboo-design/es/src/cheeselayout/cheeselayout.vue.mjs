3acffbb3dc82a75eb3e221ccfe38389a187eae2c
HandaQAQ-Code <hehanda14@gmail.com>
2025-11-16 12:42:34 +0800

    Release 2.0.24

--- packages/bamboo-design/es/src/cheeselayout/cheeselayout.vue.mjs
+++ packages/bamboo-design/es/src/cheeselayout/cheeselayout.vue.mjs
@@ -1,12 +1,15 @@
-import { defineComponent as v, ref as r, onMounted as I, watch as m, computed as u, createElementBlock as c, openBlock as i, normalizeStyle as $, normalizeClass as g, unref as o, renderSlot as B, Fragment as S, renderList as b } from "vue";
+import { defineComponent as f, ref as n, onMounted as I, watch as v, computed as i, createElementBlock as p, openBlock as u, createElementVNode as $, normalizeStyle as g, normalizeClass as b, unref as o, renderSlot as B, Fragment as S, renderList as k } from "vue";
 /* empty css                  */
-const k = {
+const C = {
+  class: "bm-cheese-container",
+  style: { width: "100%", height: "100%" }
+}, x = {
   class: "bm-cheese-dev",
   style: { width: "100%", height: "100%" }
-}, C = v({
+}, z = f({
   name: "bm-cheese"
-}), F = /* @__PURE__ */ v({
-  ...C,
+}), M = /* @__PURE__ */ f({
+  ...z,
   props: {
     cut: {},
     pos: {},
@@ -16,38 +19,39 @@ const k = {
     fill: { type: Boolean }
   },
   setup(h) {
-    let d = r(1), f = r(1), n = r(1), l = r(1), s = r(1), p = r(1);
+    let d = n(1), m = n(1), r = n(1), s = n(1), l = n(1), c = n(1);
     I(() => {
-      if (m(() => t.pos, (e, a) => {
+      if (v(() => t.pos, (e, a) => {
         e !== a && window.location.reload();
-      }), m(() => t.cut, (e, a) => {
+      }), v(() => t.cut, (e, a) => {
         e !== a && window.location.reload();
       }), t.pos) {
         const e = t.pos.split(" ");
-        n.value = parseInt(e[2]), l.value = parseInt(e[3]), d.value = parseInt(e[0]), f.value = parseInt(e[1]);
+        r.value = parseInt(e[2]), s.value = parseInt(e[3]), d.value = parseInt(e[0]), m.value = parseInt(e[1]);
       }
       if (t.cut) {
         const e = t.cut.split(" ");
-        s.value = parseInt(e[0]), p.value = parseInt(e[1]);
+        l.value = parseInt(e[0]), c.value = parseInt(e[1]);
       }
     });
-    var w = u(() => n.value ? n.value : 1), y = u(() => l.value ? l.value : 1);
-    const t = h, _ = u(() => ({
+    var w = i(() => r.value ? r.value : 1), y = i(() => s.value ? s.value : 1);
+    const t = h, _ = i(() => ({
       "bm-cheese-dev": t.dev
     }));
-    return (e, a) => (i(), c("div", {
-      ref: "cheese",
-      class: g(["bm-cheese", _.value]),
-      style: $(`
-    grid-template-columns: repeat(${o(p)}, 1fr); 
-    grid-template-rows: repeat(${o(s)}, 1fr);
-    grid-area: ${o(d) || 1}/${o(f) || 1}/span ${o(w)}/span ${o(y)};
+    return (e, a) => (u(), p("div", C, [
+      $("div", {
+        class: b(["bm-cheese", _.value]),
+        style: g(`
+    grid-template-columns: repeat(${o(c)}, 1fr); 
+    grid-template-rows: repeat(${o(l)}, 1fr);
+    grid-area: ${o(d) || 1}/${o(m) || 1}/span ${o(w)}/span ${o(y)};
     `)
-    }, [
-      t.fill ? (i(!0), c(S, { key: 1 }, b(o(s) * o(p), (x) => (i(), c("div", k))), 256)) : B(e.$slots, "default", { key: 0 })
-    ], 6));
+      }, [
+        t.fill ? (u(!0), p(S, { key: 1 }, k(o(l) * o(c), (E) => (u(), p("div", x))), 256)) : B(e.$slots, "default", { key: 0 })
+      ], 6)
+    ]));
   }
 });
 export {
-  F as default
+  M as default
 };
--- packages/bamboo-design/es/src/index.mjs
+++ packages/bamboo-design/es/src/index.mjs
@@ -2,14 +2,16 @@
 import { Button as e } from "./button/index.mjs";
 import { Input as m } from "./input/index.mjs";
 import { Icon as x } from "./Icon/index.mjs";
-import { Pin as u } from "./pin/index.mjs";
+import { Pin as i } from "./pin/index.mjs";
 import { PinGroup as I } from "./pinGroup/index.mjs";
 import { CheeseLayout as a } from "./cheeselayout/index.mjs";
+import { Slider as d } from "./slider/index.mjs";
 export {
   e as Button,
   a as CheeseLayout,
   x as Icon,
   m as Input,
-  u as Pin,
-  I as PinGroup
+  i as Pin,
+  I as PinGroup,
+  d as Slider
 };
